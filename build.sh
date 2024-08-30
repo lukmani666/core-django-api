47f3554b051e75ae72c46fed6d6b5b1c7a0393eb
ola-dev <olamidelukman3@gmail.com>
2024-08-30 16:01:53 +0100

    core-api-new-commimg

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ source venv/bin/activate
 pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput
