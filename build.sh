4c297fd40b17893d0f05f81343a7cc98051795fc
ola-dev <olamidelukman3@gmail.com>
2024-08-30 16:41:47 +0100

    core-api-new-commimnmtjfjrkflk

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ python3.9 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
+sleep 10
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic --noinput
