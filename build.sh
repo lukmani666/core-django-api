16093b9cfb44ec00ca4853531c304f232a4f9b73
ola-dev <olamidelukman3@gmail.com>
2024-08-30 15:43:31 +0100

    core-api-new-commituhgflkgj

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-python3.10 -m venv venv
+python3.9 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-python3.10 manage.py makemigrations
-python3.10 manage.py migrate 
-python3.10 manage.py collectstatic
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
